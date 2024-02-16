1778f15ddd209541fbea893dd233bde2883c2f2d
Yuta.Matsuo <126228724+YuutaMatsuo@users.noreply.github.com>
2024-02-16 17:36:24 +0000

    テストコード

--- src/index.php
+++ src/index.php
@@ -1,4 +1,33 @@
-<?php
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>本日のランチ</title>
+        <link rel="stylesheet" href="">
+    </head>
 
-echo '<h1>Hello World</h1>';
-echo '<h2>ハローワールド</h2>';
+    <?php
+    $meat = "チキン南蛮香味だれ";
+    $fish = "鯖の竜田揚げ";
+    ?>
+
+    <body>
+        <div class="main-contents">
+
+        本日のランチ、肉料理は
+        <h1>
+            <?php echo $meat;?>
+        </h1>
+        魚料理は、
+        <h1>
+            <?php echo $fish;?>
+        </h1>
+        です。<br>
+
+        </div>
+    </body>
+</html>
+
+
+
+
