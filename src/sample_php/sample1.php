620f27f7d14e8bfdd53221b51f950ea8f950243a
yuta <kuro1152@yahoo.co.jp>
2024-02-17 18:02:48 +0900

    bbs complete

---
+++ src/sample_php/sample1.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>本日のランチ</title>
+        <link rel="stylesheet" href="">
+    </head>
+
+    <?php
+    $meat = "チキン南蛮香味だれ";
+    $fish = "鯖の竜田揚げ";
+    ?>
+
+    <body>
+        <div class="main-contents">
+
+        本日のランチ、肉料理は
+        <h1>
+            <?php echo $meat;?>
+        </h1>
+        魚料理は、
+        <h1>
+            <?php echo $fish;?>
+        </h1>
+        です。<br>
+
+        </div>
+    </body>
+</html>
+
+
+
+
